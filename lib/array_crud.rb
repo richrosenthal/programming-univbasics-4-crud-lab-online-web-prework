0b4c6b6126ea560df0db48c3c86afdefc69f6239
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-10-28 03:48:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  array = ["apple", "peaches", "pumpkin pie", "cat food"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["apply", "peaches"]
+  pig << array
 end
 
 def add_element_to_start_of_array(array, element)
